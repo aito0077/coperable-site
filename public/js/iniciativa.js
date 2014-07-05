0b23c964f25db4790b552c4a6bb28c5b8c7c16d4
root <you@example.com>
2014-07-05 09:53:57 -0300

    Una ventana en las marcas del mapa. -lg

--- public/js/iniciativa.js
+++ public/js/iniciativa.js
@@ -688,6 +688,8 @@ Backbone.emulateHTTP = true;
       var self = this;
       this.clear_markers();
 
+        var infowindow = new google.maps.InfoWindow();
+
       _.each(this.iniciativas.models, function(model) {
         var location = model.get('location');
         var marker = new google.maps.Marker({
@@ -696,14 +698,21 @@ Backbone.emulateHTTP = true;
           map: self.map
         });
 
+        /*
         marker.info = new google.maps.InfoWindow({
           content:self.markerTemplate(_.extend({
             profile_picture: '',
             goal: ''
         }, model.toJSON()))
         });
+        */
         google.maps.event.addListener(marker, 'click', function(){
-          marker.info.open(self.map, marker);
+            infowindow.setContent(
+              self.markerTemplate(_.extend({
+                profile_picture: '',
+                goal: ''
+            }, model.toJSON())));
+            infowindow.open(self.map, marker);
         });
         self.markers.push(marker);
 
