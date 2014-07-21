d1ad246a71e79c23ace4844389f9e06b4ee90fe5
Matias Niklison <soychangoman@gmail.com>
2014-07-21 19:04:53 -0300

    * Fixed bug.

--- routes/home.js
+++ routes/home.js
@@ -3,35 +3,13 @@ var iniciativas = require('../logic/iniciativas');
 
 exports.index = function(req, res) {
 
-  /*
-  console.dir(req.cookies);
-
-  var geo = (req.session ? req.session.geo : false) || (req.cookies ? req.cookies.geo : undefined);
-  res.locals = {
-    user: req.user,
-    geo: geo,
-    title: 'Home'
-  };
-  */
-
-  /*
-  return res.render('home/index',{
-    partials: {
-      head: 'wrapper/header',
-      menu_site: 'wrapper/menu_site',
-      footer: 'wrapper/footer',
-      iniciativas_browser_command: 'wrapper/iniciativas_browser_commands',
-      map: 'widgets/map',
-      login_modal: 'user/login_widget'
-    }
-  });
-  */
   var geo = (req.session ? req.session.geo : false) ||
             (req.cookies ? req.cookies.geo : undefined);
 
   return res.render('home/index.html', {
     layoutTitle: 'Coperable - Organizá y participa de iniciativas comunitarias y colaborativas',
     layoutId: 'home-index',
+    javascipts: ['map-browser.js'],
     user: req.user,
     geo: geo
   })
