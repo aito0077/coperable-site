05375bd7c1b3fc53166fb615239882950187b761
Aito <aito0077@gmail.com>
2015-07-25 06:59:53 -0400

    Coperable V2. Home. -lg

--- public/sites/coperable/assets/js/main.js
+++ public/sites/coperable/assets/js/main.js
@@ -67,4 +67,20 @@ $(document).ready(function(){
     $container.isotope({ filter: selector });
     return false;
   });
+
+	//open the lateral panel
+	$('.cd-btn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel').addClass('is-visible');
+	});
+	//clode the lateral panel
+	$('.cd-panel').on('click', function(event){
+		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
+
+
+
 }); 
